eaf050fbefcb46c189fe28d97d50da86bd00ab3d
Shaik Sameer <130442189+Shaik-Sameer77@users.noreply.github.com>
2023-06-06 09:07:59 +0530

    description added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Board from "./Components/Board/Board";
+import Description from "./Components/Description/Description";
 
 const App = () => {
   return (
     <div>
       <Board />
+      <Description/>
     </div>
   );
 };
